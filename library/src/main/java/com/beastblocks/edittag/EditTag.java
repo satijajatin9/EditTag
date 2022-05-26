8894ab6d225a66991c3d4c297ba61f741d761b7b
satijajatin9 <satijajatin9@gmail.com>
2022-05-26 16:18:34 +0530

    Update

--- library/src/main/java/com/beastblocks/edittag/EditTag.java
+++ library/src/main/java/com/beastblocks/edittag/EditTag.java
@@ -51,6 +51,7 @@ public class EditTag extends FrameLayout
     private EditText editText;
 
     private int tagViewLayoutRes;
+    private boolean alwaysvisible=false;
 
     private int inputTagLayoutRes;
 
@@ -107,6 +108,10 @@ public class EditTag extends FrameLayout
                 mTypedArray.getResourceId(R.styleable.EditTag_delete_mode_bg, R.color.colorAccent);
         mTypedArray.recycle();
         setupView();
+    }
+
+    public void setCancelButtonVisibility(Boolean visibility){
+        alwaysvisible=visibility;
     }
 
     private void setupView() {
@@ -179,7 +184,209 @@ public class EditTag extends FrameLayout
                         defaultTagBg = tagTextView.getBackground();
                     }
                     tagTextView.setOnClickListener(EditTag.this);
+                        tagTextView.setOnTouchListener(new OnTouchListener() {
+                            @Override
+                            public boolean onTouch(View view, MotionEvent motionEvent) {
+                                Rect bounds;
+                                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                                    int actionX = (int) motionEvent.getX();
+                                    int actionY = (int) motionEvent.getY();
+                                    Drawable drawableRight = tagTextView.getCompoundDrawables()[2];
+                                    if (drawableRight != null) {
 
+                                        bounds = null;
+                                        bounds = drawableRight.getBounds();
+
+                                        int x, y;
+                                        int extraTapArea = 13;
+
+                                        /**
+                                         * IF USER CLICKS JUST OUT SIDE THE RECTANGLE OF THE DRAWABLE
+                                         * THAN ADD X AND SUBTRACT THE Y WITH SOME VALUE SO THAT AFTER
+                                         * CALCULATING X AND Y CO-ORDINATE LIES INTO THE DRAWBABLE
+                                         * BOUND. - this process help to increase the tappable area of
+                                         * the rectangle.
+                                         */
+                                        x = (int) (actionX + extraTapArea);
+                                        y = (int) (actionY - extraTapArea);
+
+                                        /**Since this is right drawable subtract the value of x from the width
+                                         * of view. so that width - tappedarea will result in x co-ordinate in drawable bound.
+                                         */
+                                        x = tagTextView.getWidth() - x;
+
+                                        /*x can be negative if user taps at x co-ordinate just near the width.
+                                         * e.g views width = 300 and user taps 290. Then as per previous calculation
+                                         * 290 + 13 = 303. So subtract X from getWidth() will result in negative value.
+                                         * So to avoid this add the value previous added when x goes negative.
+                                         */
+
+                                        if (x <= 0) {
+                                            x += extraTapArea;
+                                        }
+
+                                        /* If result after calculating for extra tappable area is negative.
+                                         * assign the original value so that after subtracting
+                                         * extratapping area value doesn't go into negative value.
+                                         */
+
+                                        if (y <= 0)
+                                            y = actionY;
+
+                                        /**If drawble bounds contains the x and y points then move ahead.*/
+                                        if (bounds.contains(x, y)) {
+                                            lastSelectTagView = tagTextView;
+                                            isDelAction = true;
+                                            removeSelectedTag();
+                                            return false;
+                                        }
+                                        return EditTag.super.onTouchEvent(motionEvent);
+                                    }
+
+                                }
+                                return EditTag.super.onTouchEvent(motionEvent);
+                            }
+                        });
+                    flowLayout.addView(tagTextView, flowLayout.getChildCount() - 1);
+                    tagValueList.add(tagContent);
+                    // reset action status
+                    editText.getText().clear();
+                    editText.performClick();
+                    isDelAction = false;
+                    isHandle = true;
+                }
+            }
+        }
+        return isHandle;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getTag() == null && isEditableStatus) {
+            // TextView tag click
+            if (lastSelectTagView == null) {
+                lastSelectTagView = (TextView) view;
+                view.setBackgroundDrawable(getDrawableByResId(deleteModeBgRes));
+                if (!alwaysvisible) {
+                    for (int i = 0; i < (flowLayout.getChildCount()); i++) {
+                        if (flowLayout.getChildAt(i) == view) {
+                            ((TextView) flowLayout.getChildAt(i)).setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_cancel_24, 0);
+                            ((TextView) flowLayout.getChildAt(i)).setCompoundDrawablePadding(8);
+
+                        } else {
+                            ((TextView) flowLayout.getChildAt(i)).setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                            ((TextView) flowLayout.getChildAt(i)).setCompoundDrawablePadding(0);
+                        }
+                    }
+                }
+            } else {
+                if (lastSelectTagView.equals(view)) {
+                    lastSelectTagView.setBackgroundDrawable(defaultTagBg);
+                    lastSelectTagView = null;
+                    if (!alwaysvisible) {
+                        ((TextView) view).setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                        ((TextView) view).setCompoundDrawablePadding(0);
+                    }
+                } else {
+                    lastSelectTagView.setBackgroundDrawable(defaultTagBg);
+                    lastSelectTagView = (TextView) view;
+                    view.setBackgroundDrawable(getDrawableByResId(deleteModeBgRes));
+                    if (!alwaysvisible) {
+                        for (int i = 0; i < (flowLayout.getChildCount()); i++) {
+                            if (flowLayout.getChildAt(i) == view) {
+                                ((TextView) flowLayout.getChildAt(i)).setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_cancel_24, 0);
+                                ((TextView) flowLayout.getChildAt(i)).setCompoundDrawablePadding(8);
+
+                            } else {
+                                ((TextView) flowLayout.getChildAt(i)).setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                                ((TextView) flowLayout.getChildAt(i)).setCompoundDrawablePadding(0);
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            // EditText tag click
+            if (lastSelectTagView != null) {
+                lastSelectTagView.setBackgroundDrawable(defaultTagBg);
+                lastSelectTagView = null;
+            }
+        }
+    }
+
+    private void removeSelectedTag() {
+        int size = tagValueList.size();
+        if (size > 0 && lastSelectTagView != null) {
+            int index = flowLayout.indexOfChild(lastSelectTagView);
+            tagValueList.remove(index);
+            flowLayout.removeView(lastSelectTagView);
+            if (tagDeletedCallback != null) {
+                tagDeletedCallback.onTagDelete(lastSelectTagView.getText().toString());
+            }
+            lastSelectTagView = null;
+            isDelAction = false;
+        }
+    }
+
+    private TextView createTag(ViewGroup parent, String s) {
+        TextView tagTv = (TextView) LayoutInflater.from(getContext()).inflate(tagViewLayoutRes, parent, false);
+        //TextView tagTv = new TextView(getContext());
+        if (alwaysvisible) {
+            tagTv.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_cancel_24, 0);
+            tagTv.setCompoundDrawablePadding(8);
+        }
+        tagTv.setText(s);
+        return tagTv;
+    }
+
+    private EditText createInputTag(ViewGroup parent) {
+        editText =
+                (EditText) LayoutInflater.from(getContext()).inflate(inputTagLayoutRes, parent, false);
+        return editText;
+    }
+
+    private void addTagView(List<String> tagList) {
+        int size = tagList.size();
+        for (int i = 0; i < size; i++) {
+            addTag(tagList.get(i));
+        }
+    }
+
+    private Drawable getDrawableByResId(int resId) {
+        return getContext().getResources().getDrawable(resId);
+    }
+
+    public void setEditable(boolean editable) {
+        if (editable) {
+            if (!isEditableStatus) {
+                flowLayout.addView((editText));
+            }
+        } else {
+            int childCount = flowLayout.getChildCount();
+            if (isEditableStatus && childCount > 0) {
+                flowLayout.removeViewAt(childCount - 1);
+                if (lastSelectTagView != null) {
+                    lastSelectTagView.setBackgroundDrawable(defaultTagBg);
+                    isDelAction = false;
+                    editText.getText().clear();
+                }
+            }
+        }
+        this.isEditableStatus = editable;
+    }
+
+    public boolean addTag(String tagContent) {
+        if (TextUtils.isEmpty(tagContent)) {
+            // do nothing, or you can tip "can't add empty tag"
+            return false;
+        } else {
+            if (tagAddCallBack == null || (tagAddCallBack != null
+                    && tagAddCallBack.onTagAdd(tagContent))) {
+                TextView tagTextView = createTag(flowLayout, tagContent);
+                if (defaultTagBg == null) {
+                    defaultTagBg = tagTextView.getBackground();
+                }
+                tagTextView.setOnClickListener(EditTag.this);
                     tagTextView.setOnTouchListener(new OnTouchListener() {
                         @Override
                         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -243,180 +450,6 @@ public class EditTag extends FrameLayout
                             return EditTag.super.onTouchEvent(motionEvent);
                         }
                     });
-                    flowLayout.addView(tagTextView, flowLayout.getChildCount() - 1);
-                    tagValueList.add(tagContent);
-                    // reset action status
-                    editText.getText().clear();
-                    editText.performClick();
-                    isDelAction = false;
-                    isHandle = true;
-                }
-            }
-        }
-        return isHandle;
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view.getTag() == null && isEditableStatus) {
-            // TextView tag click
-            if (lastSelectTagView == null) {
-                lastSelectTagView = (TextView) view;
-                view.setBackgroundDrawable(getDrawableByResId(deleteModeBgRes));
-            } else {
-                if (lastSelectTagView.equals(view)) {
-                    lastSelectTagView.setBackgroundDrawable(defaultTagBg);
-                    lastSelectTagView = null;
-                } else {
-                    lastSelectTagView.setBackgroundDrawable(defaultTagBg);
-                    lastSelectTagView = (TextView) view;
-                    view.setBackgroundDrawable(getDrawableByResId(deleteModeBgRes));
-                }
-            }
-        } else {
-            // EditText tag click
-            if (lastSelectTagView != null) {
-                lastSelectTagView.setBackgroundDrawable(defaultTagBg);
-                lastSelectTagView = null;
-            }
-        }
-    }
-
-    private void removeSelectedTag() {
-        int size = tagValueList.size();
-        if (size > 0 && lastSelectTagView != null) {
-            int index = flowLayout.indexOfChild(lastSelectTagView);
-            tagValueList.remove(index);
-            flowLayout.removeView(lastSelectTagView);
-            if (tagDeletedCallback != null) {
-                tagDeletedCallback.onTagDelete(lastSelectTagView.getText().toString());
-            }
-            lastSelectTagView = null;
-            isDelAction = false;
-        }
-    }
-
-    private TextView createTag(ViewGroup parent, String s) {
-        TextView tagTv = (TextView) LayoutInflater.from(getContext()).inflate(tagViewLayoutRes, parent, false);
-        //TextView tagTv = new TextView(getContext());
-        tagTv.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_cancel_24, 0);
-        tagTv.setCompoundDrawablePadding(8);
-        tagTv.setText(s);
-        return tagTv;
-    }
-
-    private EditText createInputTag(ViewGroup parent) {
-        editText =
-                (EditText) LayoutInflater.from(getContext()).inflate(inputTagLayoutRes, parent, false);
-        return editText;
-    }
-
-    private void addTagView(List<String> tagList) {
-        int size = tagList.size();
-        for (int i = 0; i < size; i++) {
-            addTag(tagList.get(i));
-        }
-    }
-
-    private Drawable getDrawableByResId(int resId) {
-        return getContext().getResources().getDrawable(resId);
-    }
-
-    public void setEditable(boolean editable) {
-        if (editable) {
-            if (!isEditableStatus) {
-                flowLayout.addView((editText));
-            }
-        } else {
-            int childCount = flowLayout.getChildCount();
-            if (isEditableStatus && childCount > 0) {
-                flowLayout.removeViewAt(childCount - 1);
-                if (lastSelectTagView != null) {
-                    lastSelectTagView.setBackgroundDrawable(defaultTagBg);
-                    isDelAction = false;
-                    editText.getText().clear();
-                }
-            }
-        }
-        this.isEditableStatus = editable;
-    }
-
-    public boolean addTag(String tagContent) {
-        if (TextUtils.isEmpty(tagContent)) {
-            // do nothing, or you can tip "can't add empty tag"
-            return false;
-        } else {
-            if (tagAddCallBack == null || (tagAddCallBack != null
-                    && tagAddCallBack.onTagAdd(tagContent))) {
-                TextView tagTextView = createTag(flowLayout, tagContent);
-                if (defaultTagBg == null) {
-                    defaultTagBg = tagTextView.getBackground();
-                }
-                tagTextView.setOnClickListener(EditTag.this);
-
-                tagTextView.setOnTouchListener(new OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View view, MotionEvent motionEvent) {
-                        Rect bounds;
-                        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                            int actionX = (int) motionEvent.getX();
-                            int actionY = (int) motionEvent.getY();
-                            Drawable drawableRight = tagTextView.getCompoundDrawables()[2];
-                            if (drawableRight != null) {
-
-                                bounds = null;
-                                bounds = drawableRight.getBounds();
-
-                                int x, y;
-                                int extraTapArea = 13;
-
-                                /**
-                                 * IF USER CLICKS JUST OUT SIDE THE RECTANGLE OF THE DRAWABLE
-                                 * THAN ADD X AND SUBTRACT THE Y WITH SOME VALUE SO THAT AFTER
-                                 * CALCULATING X AND Y CO-ORDINATE LIES INTO THE DRAWBABLE
-                                 * BOUND. - this process help to increase the tappable area of
-                                 * the rectangle.
-                                 */
-                                x = (int) (actionX + extraTapArea);
-                                y = (int) (actionY - extraTapArea);
-
-                                /**Since this is right drawable subtract the value of x from the width
-                                 * of view. so that width - tappedarea will result in x co-ordinate in drawable bound.
-                                 */
-                                x = tagTextView.getWidth() - x;
-
-                                /*x can be negative if user taps at x co-ordinate just near the width.
-                                 * e.g views width = 300 and user taps 290. Then as per previous calculation
-                                 * 290 + 13 = 303. So subtract X from getWidth() will result in negative value.
-                                 * So to avoid this add the value previous added when x goes negative.
-                                 */
-
-                                if (x <= 0) {
-                                    x += extraTapArea;
-                                }
-
-                                /* If result after calculating for extra tappable area is negative.
-                                 * assign the original value so that after subtracting
-                                 * extratapping area value doesn't go into negative value.
-                                 */
-
-                                if (y <= 0)
-                                    y = actionY;
-
-                                /**If drawble bounds contains the x and y points then move ahead.*/
-                                if (bounds.contains(x, y)) {
-                                    lastSelectTagView = tagTextView;
-                                    isDelAction = true;
-                                    removeSelectedTag();
-                                    return false;
-                                }
-                                return EditTag.super.onTouchEvent(motionEvent);
-                            }
-
-                        }
-                        return EditTag.super.onTouchEvent(motionEvent);
-                    }
-                });
                 if (isEditableStatus) {
                     flowLayout.addView(tagTextView, flowLayout.getChildCount() - 1);
                 } else {
